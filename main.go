711a4cf4b396106e619f48c838fbf646a6c172ff
Pokpitch Patcharadamrongkul <pokpitch.patcharadamrongkul@stream.co.th>
2023-06-19 12:43:19 +0700

    add ping api

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func main() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&Book{})
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+}
+
+type Book struct {
+	gorm.Model
+	Name   string `json:"name"`
+	Author string `json:"author"`
+}
